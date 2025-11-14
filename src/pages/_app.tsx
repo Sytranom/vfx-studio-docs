a8f7ab57c26611b18633467cb9d4a336228568c4
Sytranom <lolster543@gmail.com>
2025-11-14 15:27:26 -0500

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { Inter, JetBrains_Mono } from "next/font/google";
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// We are removing the next/font imports
 
 config.autoAddCss = false;
 
-// Configure and load the fonts with next/font
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-sans", // CSS Variable for the sans-serif font
-});
-
-const jetbrains_mono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono", // CSS Variable for the mono font
-});
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // Add the font variables to the root HTML element
-    <div className={`${inter.variable} ${jetbrains_mono.variable}`}>
+    // The font variables are no longer needed on the div
+    <div>
       <Head>
         <title>VFX Studio Documentation</title>
-        {/* The old <link> tags for fonts are no longer needed */}
+        {/* Reverted to the classic <link> tag method for loading fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=JetBrains+Mono:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Component {...pageProps} />
     </div>
