3acab42ef8d19ca092e0d005c2d18936ab9c028a
Sytranom <lolster543@gmail.com>
2025-11-15 00:28:53 -0500

    fix stysb

--- src/pages/docs/[slug].tsx
+++ src/pages/docs/[slug].tsx
@@ -88,23 +88,18 @@ const prettyCodeOptions: Partial<Options> = {
     }
   },
   onVisitHighlightedLine(node) {
-    
     if (!node.properties) {
       node.properties = {};
     }
-    
     if (!Array.isArray(node.properties.className)) {
       node.properties.className = [];
     }
-    
     node.properties.className.push('highlighted');
   },
-  onVisitHighlightedWord(node) {
-    
+  onVisitHighlightedChars(node) {
     if (!node.properties) {
         node.properties = {};
     }
-    
     node.properties.className = ['highlighted', 'word'];
   },
 };
